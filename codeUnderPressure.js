62ca7f2a2d775f5850f6bfcc42b49a5c09cbcd18
Jasmine John <jsjohn026@gmail.com>
2024-10-21 14:39:59 -0500

    Can You Code Under Pressure?

---
+++ codeUnderPressure.js
@@ -0,0 +1,28 @@
+/*
+Description:
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i) {
+  return i + i;
+}
+
+// refactor:
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i;
+}
+
+
+// Tests:
+/* 
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(doubleInteger(2), 4);
+  });
+});
+ 
+*/
