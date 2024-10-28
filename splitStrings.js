f96623a7dac4abbe7fc4fae77123e83b9474924e
Jasmine John <jsjohn026@gmail.com>
2024-10-28 10:00:32 -0500

    Split Strings

---
+++ splitStrings.js
@@ -0,0 +1,44 @@
+/*
+Description:
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+*/
+
+function solution(str){
+   let pairs = []
+   for(let i = 0; i < str.length; i++){
+    if(i % 2 !== 0) {
+      pairs.push(str.slice(i - 1, i + 1))
+    }
+  }
+
+  if (str.length % 2 !== 0) {
+    pairs.push(str[str.length - 1] + '_')
+  }
+  
+  return pairs; 
+}
+
+// refactor:
+
+function solution(str){
+   
+}
+
+
+// Tests:
+/* 
+const { assert } = require('chai');
+
+describe("Split Strings", () => {
+  it("Basic tests", () => {
+    assert.deepEqual(solution("abcdef"), ["ab", "cd", "ef"]);
+    assert.deepEqual(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+    assert.deepEqual(solution(""), []);
+  });
+});
+*/
