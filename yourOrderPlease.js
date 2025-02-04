6c8ea2c62a51b58be315da3978b01382fa47d236
Jasmine John <jsjohn026@gmail.com>
2025-02-04 09:22:00 -0600

    Add Your Order Please

---
+++ yourOrderPlease.js
@@ -0,0 +1,51 @@
+/*
+Description:
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+
+function order(words){
+  let allWords = words.split(' ');
+  let finalWords = []
+  for (let i = 0; i < allWords.length; i++) {
+    const num = Number(allWords[i].split('').find((e) => Number(e)));
+    finalWords[num -1] = allWords[i]
+  }
+  return finalWords.join(' ')
+}
+
+// refactor:
+function order(words){
+  var array = words.split(' ');
+  var sortedArray = [];
+  for(i = 0; i <= array.length; i++) {
+    for(j = 0; j < array.length; j++) {
+      if(array[j].indexOf(i) >= 0) {
+        sortedArray.push(array[j]);
+      }
+    }
+  }
+  return sortedArray.join(' ');
+}
+
+
+// Tests:
+/* 
+const {assert} = require('chai');
+
+describe("order", () => {
+  it("should work corectly", () => {
+    assert.strictEqual(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
+    assert.strictEqual(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
+    assert.strictEqual(order(""), "", "empty input should return empty string" )
+  });
+});
+*/
