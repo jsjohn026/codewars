58c15ee303bdfc1edeea4257ab39c1cccad46422
Jasmine John <jsjohn026@gmail.com>
2025-10-17 16:11:14 -0500

    3 Sum

--- threeSum.js
+++ threeSum.js
@@ -8,6 +8,8 @@ The output should not contain any duplicate triplets. You may return the output 
 Example 1:
 
 Input: nums = [-1,0,1,2,-1,-4]
+[-1,-1,-4,0,1,2] result = []
+result = [[-1,-1,2], ]
 
 Output: [[-1,-1,2],[-1,0,1]]
 Explanation:
@@ -61,6 +63,30 @@ function threeSum(nums){
 function threeSum(nums) {
    nums.sort((a, b) => a - b)
    const result = []
+
+   for (let i = 0; i < nums.length - 2; i++) {
+      if (i > 0 && nums[i] === nums[i - 1]) continue
+
+      let left = i + 1
+      let right = nums.length - 1
+
+      while (left < right) {
+         const sum = nums[i] + nums[left] + nums[right]
+         if (sum === 0) {
+            result.push([nums[i], nums[left], nums[right]])
+            left++
+            right--
+
+            while (left < right && nums[left] === nums[left - 1]) left++ 
+            while (left < right && nums[right] === nums[right + 1]) right-- 
+         } else if (sum < 0) {
+            left++
+         } else {
+            right--
+         }
+      }
+   }
+   return result
 }
 
 
